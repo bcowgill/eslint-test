6df08b834aad05eeb423b5e47760678da7b80ce3
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-04-01 23:31:07 +0100

    no-console rules tested

---
+++ src/no-console--allow-warn-error.lint.js
@@ -0,0 +1,9 @@
+/*eslint no-console: ["error", { allow: ["warn", "error"] }] */
+
+(function () {
+
+//	console.log('Log a debug level message.')
+	console.warn('Log a warn level message.')
+	console.error('Log an error level message.')
+
+})()
---
+++ src/no-console.lint.js
@@ -0,0 +1,8 @@
+/*eslint no-console: "error"*/
+
+(function (konsole) {
+
+	// represents a custom console log
+	konsole.log('Log a debug level message.')
+
+})()
