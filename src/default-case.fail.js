199e4dbfd07e8d9a0d446681422db836f1e2590a
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-04-02 13:29:21 +0100

    default-case rule tested

---
+++ src/default-case.fail.js
@@ -0,0 +1,18 @@
+/*eslint default-case: "error"*/
+
+(function (a) {
+
+	switch (a) {
+	case 1:
+		/* code */
+		break
+	}
+
+	switch (a) {
+	case 1:
+		/* code */
+		break
+	// a comment that is not an indicator of permitting default absence
+	}
+})()
+
---
+++ src/default-case.lint.js
@@ -0,0 +1,32 @@
+/*eslint default-case: "error"*/
+
+(function (a) {
+
+	switch (a) {
+	case 1:
+		/* code */
+		break
+
+	default:
+		/* code */
+		break
+	}
+
+
+	switch (a) {
+	case 1:
+		/* code */
+		break
+
+	// no default
+	}
+
+	switch (a) {
+	case 1:
+		/* code */
+		break
+
+	// No Default
+	}
+
+})()
